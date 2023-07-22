e5a9111f1998a8cd8f40356ed50c8367b943119a
xWatchR <sebastianmissler@web.de>
2023-07-23 01:32:14 +0200

    x

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,9 @@ module.exports = {
     'eslint:recommended',
     '@vue/eslint-config-prettier',
   ],
+  rules: {
+    'prettier/prettier': 0,
+  },
   parserOptions: {
     ecmaVersion: 'latest',
   },
